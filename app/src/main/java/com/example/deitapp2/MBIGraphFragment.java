50995093ad9ac27363590e1759031f5be0af54a5
sugar2456 <sugar2456_0526@yahoo.co.jp>
2020-08-10 18:57:49 +0900

    空の体脂肪、BMI、代謝画面追加　コミット漏れ

---
+++ app/src/main/java/com/example/deitapp2/MBIGraphFragment.java
@@ -0,0 +1,26 @@
+package com.example.deitapp2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.github.mikephil.charting.charts.LineChart;
+
+public class MBIGraphFragment extends Fragment {
+    private LineChart mChart;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_mbi_graph, container, false);
+
+        mChart = rootView.findViewById(R.id.BMILineChart);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/deitapp2/MetabolismGraphFragment.java
@@ -0,0 +1,26 @@
+package com.example.deitapp2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.github.mikephil.charting.charts.LineChart;
+
+public class MetabolismGraphFragment extends Fragment {
+    private LineChart mChart;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_metabolism_graph, container, false);
+
+        mChart = rootView.findViewById(R.id.MetabolismLineChart);
+
+        return rootView;
+    }
+}
